9c1828896c0b26cd02fa9587353b62cdcdc9809c
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-01-21 18:16:09 -0600

    Add More Sprite Utility Methods

--- include/cmdfx/sprites.h
+++ include/cmdfx/sprites.h
@@ -49,7 +49,7 @@ typedef struct CmdFX_Sprite {
      * @brief The ANSI data for the sprite.
      * 
      * This is a 2D array of strings that represent the ANSI codes for the sprite.
-     * The string is prefixed with "\033[" and suffixed with "m". Each row should
+     * Strings must be prefixed with "\033[" and suffixed with "m". Each row should
      * be the same length, and the number of rows should be equal to the height of
      * the sprite. The sprite will be drawn starting at the X and Y position, with
      * the top-left corner of the sprite at the X and Y position. The number of
@@ -89,34 +89,141 @@ CmdFX_Sprite** Canvas_getDrawnSprites();
  * @brief Creates a new sprite.
  * 
  * This method allocates memory for a new sprite and initializes it with the
- * given parameters. The sprite should be freed with `free` when it is no longer
+ * given parameters. The sprite should be freed with `Sprite_free` when it is no longer
  * needed.
+ * 
+ * You can also set `text` and `ansi` to NULL and built them later using `Sprite_setChar`, `Sprite_setAnsi`, and `Sprite_resize`.
  * 
  * This method does not draw the sprite to the terminal. To draw the sprite, use
  * the `Sprite_draw` method.
- * @param x The X position of the sprite.
- * @param y The Y position of the sprite.
- * @param width The width of the sprite.
- * @param height The height of the sprite.
  * @param text The text data for the sprite.
  * @param ansi The ANSI data for the sprite.
  * @param z The Z-index of the sprite.
  * @return A pointer to the new sprite, or 0 if an error occurred.
  */
-CmdFX_Sprite* Sprite_create(int x, int y, int width, int height, char** text, char*** ansi, int z);
+CmdFX_Sprite* Sprite_create(char** text, char*** ansi, int z);
 
 /**
- * Draws a sprite to the terminal. If the sprite is already drawn, it will be
- * redrawn at its current position.
- * @param sprite The sprite to draw.
+ * Frees the memory allocated for a sprite. If the sprite is currently drawn, it will be removed.
+ * @param sprite The sprite to free.
  */
-void Sprite_draw(CmdFX_Sprite* sprite);
+void Sprite_free(CmdFX_Sprite* sprite);
+
+/**
+ * @brief Draws a sprite to the terminal.
+ * 
+ * This method draws a sprite to the terminal at the given position. The sprite
+ * will be drawn starting at the X and Y position, with the top-left corner of
+ * the sprite at the X and Y position. The sprite will be drawn on top of any
+ * existing content at that position, according to its Z-index. If it is already
+ * drawn, it will be redrawn at the new position.
+ * 
+ * If `data` is NULL, the sprite will not be drawn.
+ * @param x The X position of the sprite.
+ * @param y The Y position of the sprite.
+ * @param sprite The sprite to draw.
+ * @return 1 if the sprite was drawn, 0 if not or an error occurred.
+ */
+int Sprite_draw(int x, int y, CmdFX_Sprite* sprite);
 
 /**
  * Removes a sprite from the terminal, making it no longer visble.
  * @param sprite The sprite to remove.
  */
 void Sprite_remove(CmdFX_Sprite* sprite);
+
+// Utility Methods - Sprite Builder
+
+/**
+ * @brief Sets the character at the given position in the sprite.
+ * 
+ * This method sets the character at the given position in the sprite. The
+ * position is specified by the X and Y coordinates, where (0, 0) is the
+ * top-left corner of the sprite. If the position is out of bounds, the
+ * method will return 0.
+ * @param sprite The sprite to modify.
+ * @param x The X position in the sprite.
+ * @param y The Y position in the sprite.
+ * @param c The character to set.
+ * @return 1 if the character was set, 0 if the position is out of bounds or an error occurred.
+ */
+int Sprite_setChar(CmdFX_Sprite* sprite, int x, int y, char c);
+
+/**
+ * @brief Sets the ANSI code at the given position in the sprite.
+ * 
+ * This method sets the ANSI code at the given position in the sprite. The
+ * position is specified by the X and Y coordinates, where (0, 0) is the
+ * top-left corner of the sprite. If the position is out of bounds, the
+ * method will return 0. The ANSI code will automatically be prefixed with "\033["
+ * and suffixed with "m".
+ * @param sprite The sprite to modify.
+ * @param x The X position in the sprite.
+ * @param y The Y position in the sprite.
+ * @param ansi The ANSI code to set.
+ * @return 1 if the ANSI code was set, 0 if the position is out of bounds or an error occurred.
+ */
+int Sprite_setAnsi(CmdFX_Sprite* sprite, int x, int y, const char* ansi);
+
+/**
+ * @brief Resizes the sprite to the given dimensions.
+ * 
+ * This method is functionally identical to `Sprite_resizeWithPadding`, but
+ * it will pad the sprite with spaces if it is resized larger.
+ * 
+ * This method will also redraw the sprite if it is currently drawn.
+ * @param sprite The sprite to resize.
+ * @param width The new width of the sprite.
+ * @param height The new height of the sprite.
+ * @return 1 if the sprite was resized, 0 if an error occurred.
+ */
+int Sprite_resize(CmdFX_Sprite* sprite, int width, int height);
+
+/**
+ * @brief Resizes the sprite to the given dimensions.
+ * 
+ * This method resizes the sprite to the given width and height. If the new
+ * dimensions are smaller than the current dimensions, the sprite will be
+ * cropped from the top left. If the new dimensions are larger than the current dimensions, the
+ * sprite will be padded with the provided character. If an error occurs, the method will return 0.
+ * 
+ * This method will also redraw the sprite if it is currently drawn.
+ * @param sprite The sprite to resize.
+ * @param width The new width of the sprite.
+ * @param height The new height of the sprite.
+ * @param padding The character to pad the sprite with if it is resized larger.
+ * @return 1 if the sprite was resized, 0 if an error occurred.
+ */
+int Sprite_resizeWithPadding(CmdFX_Sprite* sprite, int width, int height, char padding);
+
+/**
+ * @brief Centers the sprite on the terminal.
+ * 
+ * This method centers the sprite on the terminal, both horizontally and
+ * vertically. It will reposition the `data` and `ansi` characters depending
+ * on whether there is whitespace surrounding other characters.
+ * 
+ * The sprite will be drawn at its current position.
+ * @param sprite The sprite to center.
+ * @return 1 if the sprite was centered, 0 if an error occurred.
+ */
+int Sprite_center(CmdFX_Sprite* sprite);
+
+/**
+ * @brief Resizes and centers the sprite on the terminal.
+ * 
+ * This method resizes the sprite to the given width and height, and then
+ * centers the sprite on the terminal, both horizontally and vertically. It
+ * will reposition the `data` and `ansi` characters depending on whether there
+ * is whitespace surrounding other characters.
+ * 
+ * The sprite will be drawn at its new position.
+ * @param sprite The sprite to resize and center.
+ * @param width The new width of the sprite.
+ * @param height The new height of the sprite.
+ * @return 1 if the sprite was resized and centered, 0 if an error occurred.
+ */
+int Sprite_resizeAndCenter(CmdFX_Sprite* sprite, int width, int height);
 
 // Utility Methods - Movement
 
--- src/common/sprites.c
+++ src/common/sprites.c
@@ -12,40 +12,77 @@ CmdFX_Sprite** Canvas_getDrawnSprites() {
     return sprites;
 }
 
-CmdFX_Sprite* Sprite_create(int x, int y, int width, int height, char** data, char*** ansi, int z) {
-    if (data == 0) return 0;
-    if (x < 1 || y < 1) return 0;
-    if (width < 1 || height < 1) return 0;
+void _getSpriteDimensions(char** data, int* width, int* height) {
+    *width = 0;
+    *height = 0;
 
-    int rows = sizeof(data) / sizeof(data[0]);
-    if (rows != height) return 0;
+    if (data == 0) return;
 
-    for (int i = 0; i < rows; i++) {
-        if (strlen(data[i]) != width) return 0;
+    for (int i = 0; data[i] != 0; i++) {
+        (*height)++;
+        int len = strlen(data[i]);
+        if (len > *width) {
+            *width = len;
+        }
     }
+}
 
+CmdFX_Sprite* Sprite_create(char** data, char*** ansi, int z) {
     CmdFX_Sprite* sprite = malloc(sizeof(CmdFX_Sprite));
     if (sprite == 0) return 0;
 
-    sprite->x = x;
-    sprite->y = y;
-    sprite->width = width;
-    sprite->height = height;
-    sprite->data = data;
-    sprite->ansi = ansi;
+    sprite->x = -1;
+    sprite->y = -1;
     sprite->z = z;
     sprite->id = 0;
+
+    _getSpriteDimensions(data, &sprite->width, &sprite->height);
+    sprite->data = data;
+    sprite->ansi = ansi;
 
     return sprite;
 }
 
+void Sprite_free(CmdFX_Sprite* sprite) {
+    if (sprite == 0) return;
+    if (sprite->id != 0) Sprite_remove(sprite);
+
+    for (int i = 0; i < sprite->height; i++) {
+        if (sprite->data[i] == 0) continue;
+
+        free(sprite->data[i]);
+
+        if (sprite->ansi != 0) {
+            char** ansi = sprite->ansi[i];
+            if (ansi == 0) continue;
+
+            char* line = ansi[0];
+            while (line != 0) {
+                free(line);
+                line++;
+            }
+            free(ansi);
+        }
+    }
+
+    free(sprite->data);
+    if (sprite->ansi != 0) free(sprite->ansi);
+    free(sprite);
+}
+
 void Sprite_draw0(CmdFX_Sprite* sprite) {
+    if (sprite->data == 0) return;
+
     int oldX = Canvas_getCursorX();
     int oldY = Canvas_getCursorY();
 
     for (int i = 0; i < sprite->height; i++) {
+        char* line = sprite->data[i];
+        if (line == 0) continue;
+
         for (int j = 0; j < sprite->width; j++) {
-            char* ansi = sprite->ansi[i][j];
+            char c = line[j];
+            if (c == 0 || c == ' ') continue;
 
             int x = sprite->x + j;
             int y = sprite->y + i;
@@ -62,24 +99,35 @@ void Sprite_draw0(CmdFX_Sprite* sprite) {
             }
             if (skip) continue;
 
+            int hasAnsi = sprite->ansi != 0;
             Canvas_setCursor(x, y);
-            Canvas_setAnsi(x, y, ansi);
-            putchar(sprite->data[i][j]);
-            Canvas_resetFormat();
+            if (hasAnsi) {
+                char* ansi = sprite->ansi[i][j];
+                Canvas_setAnsi(x, y, ansi);
+            }
+            putchar(c);
+            if (hasAnsi) Canvas_resetFormat();
         }
     }
 
     Canvas_setCursor(oldX, oldY);
 }
 
-void Sprite_draw(CmdFX_Sprite* sprite) {
+int Sprite_draw(int x, int y, CmdFX_Sprite* sprite) {
+    if (sprite == 0) return 0;
+    if (sprite->data == 0) return 0;
+    if (x < 1 || y < 1) return 0;
+
+    sprite->x = x;
+    sprite->y = y;
+
     if (sprite->id != 0) {
         CmdFX_Sprite* old = sprites[sprite->id - 1];
 
         // Redraw Sprite
         *old = *sprite;
         Sprite_draw0(old);
-        return;
+        return 1;
     }
 
     // Draw Sprite
@@ -90,7 +138,7 @@ void Sprite_draw(CmdFX_Sprite* sprite) {
         sprites = malloc(sizeof(CmdFX_Sprite*));
         if (!sprites) {
             free(sprite);
-            return;
+            return 0;
         }
 
         sprite->id = 1;
@@ -100,7 +148,7 @@ void Sprite_draw(CmdFX_Sprite* sprite) {
         CmdFX_Sprite** temp = realloc(sprites, sizeof(CmdFX_Sprite*) * (spriteCount + 2));
         if (!temp) {
             free(sprite);
-            return;
+            return 0;
         }
         sprites = temp;
 
@@ -108,6 +156,8 @@ void Sprite_draw(CmdFX_Sprite* sprite) {
         sprites[spriteCount] = sprite;
         sprites[spriteCount + 1] = 0;
     }
+
+    return 1;
 }
 
 void Sprite_remove0(CmdFX_Sprite* sprite) {
@@ -144,6 +194,229 @@ void Sprite_remove(CmdFX_Sprite* sprite) {
     sprites = temp;
 
     spriteCount--;
+}
+
+// Utility Methods - Sprite Builder
+
+int Sprite_setChar(CmdFX_Sprite* sprite, int x, int y, char c) {
+    if (sprite == 0) return 0;
+    if (sprite->data == 0) return 0;
+    if (x < 0 || y < 0 || x >= sprite->width || y >= sprite->height) return 0;
+
+    sprite->data[y][x] = c;
+    return 1;
+}
+
+int Sprite_setAnsi(CmdFX_Sprite* sprite, int x, int y, const char* ansi) {
+    if (sprite == 0) return 0;
+    if (sprite->ansi == 0) return 0;
+    if (x < 0 || y < 0 || x >= sprite->width || y >= sprite->height) return 0;
+
+    char* ansi0 = malloc(strlen(ansi) + 6);
+    sprintf(ansi0, "\033[%sm", ansi);
+
+    sprite->ansi[y][x] = ansi0;
+    return 1;
+}
+
+int Sprite_resize0(CmdFX_Sprite* sprite, int width, int height, char padding) {
+    if (sprite->data == 0) {
+        sprite->data = malloc(sizeof(char*) * sprite->height);
+        if (sprite->data == 0) return 0;
+        for (int i = 0; i < sprite->height; i++) {
+            sprite->data[i] = malloc(sizeof(char) * sprite->width);
+            if (sprite->data[i] == 0) return 0;
+            memset(sprite->data[i], padding, sprite->width);
+        }
+    }
+
+    char** newData = malloc(sizeof(char*) * height);
+    if (newData == 0) return 0;
+
+    char*** newAnsi = 0;
+    if (sprite->ansi != 0) {
+        newAnsi = malloc(sizeof(char**) * height);
+        if (newAnsi == 0) {
+            free(newData);
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < height; i++) {
+        newData[i] = malloc(sizeof(char) * width);
+
+        // Fail check
+        if (newData[i] == 0) {
+            for (int j = 0; j < i; j++) free(newData[j]);
+            free(newData);
+            if (newAnsi != 0) {
+                for (int j = 0; j < i; j++) free(newAnsi[j]);
+                free(newAnsi);
+            }
+            return 0;
+        }
+
+        memset(newData[i], padding, width);
+
+        if (newAnsi != 0) {
+            newAnsi[i] = malloc(sizeof(char*) * width);
+
+            // Fail check
+            if (newAnsi[i] == 0) {
+                for (int j = 0; j <= i; j++) {
+                    free(newData[j]);
+                }
+                free(newData);
+
+                for (int j = 0; j < i; j++) {
+                    free(newAnsi[j]);
+                    newData[i][j] = (sprite->data[i] != 0) ? sprite->data[i][j] : padding;
+                }
+
+                free(newAnsi);
+            }
+
+            for (int j = 0; j < width; j++) newAnsi[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < sprite->height && i < height; i++) {
+        for (int j = 0; j < sprite->width && j < width; j++) {
+            newData[i][j] = sprite->data[i][j];
+            if (newAnsi != 0 && sprite->ansi != 0) {
+                newAnsi[i][j] = sprite->ansi[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < sprite->height; i++) {
+        free(sprite->data[i]);
+        if (sprite->ansi != 0) {
+            for (int j = 0; j < sprite->width; j++) {
+                if (sprite->ansi[i][j] != 0) free(sprite->ansi[i][j]);
+            }
+            free(sprite->ansi[i]);
+        }
+    }
+
+    free(sprite->data);
+    if (sprite->ansi != 0) free(sprite->ansi);
+
+    sprite->data = newData;
+    sprite->ansi = newAnsi;
+    sprite->width = width;
+    sprite->height = height;
+}
+
+int Sprite_resize(CmdFX_Sprite* sprite, int width, int height) {
+    return Sprite_resizeWithPadding(sprite, width, height, ' ');
+}
+
+int Sprite_resizeWithPadding(CmdFX_Sprite* sprite, int width, int height, char padding) {
+    if (sprite == 0) return 0;
+    if (width < 1 || height < 1) return 0;
+
+    // Remove Sprite if Drawn
+    if (sprite->id != 0) Sprite_remove0(sprite);
+
+    if (!Sprite_resize0(sprite, width, height, padding)) {
+        Sprite_draw0(sprite); // Redraw, resize failed
+        return 0;
+    }
+
+    // Redraw Sprite if Drawn
+    if (sprite->id != 0) Sprite_draw0(sprite);
+
+    return 1;
+}
+
+int Sprite_center0(CmdFX_Sprite* sprite) {
+    int left = sprite->width, right = 0, top = sprite->height, bottom = 0;
+
+    // Find the bounding box of non-whitespace characters
+    for (int i = 0; i < sprite->height; i++) {
+        for (int j = 0; j < sprite->width; j++) {
+            if (sprite->data[i][j] != ' ') {
+                if (j < left) left = j;
+                if (j > right) right = j;
+                if (i < top) top = i;
+                if (i > bottom) bottom = i;
+            }
+        }
+    }
+
+    int newWidth = right - left + 1;
+    int newHeight = bottom - top + 1;
+    int offsetX = (sprite->width - newWidth) / 2;
+    int offsetY = (sprite->height - newHeight) / 2;
+
+    // Move the contents of data and ansi toward the center
+    for (int i = 0; i < newHeight; i++) {
+        for (int j = 0; j < newWidth; j++) {
+            sprite->data[offsetY + i][offsetX + j] = sprite->data[top + i][left + j];
+            if (sprite->ansi != 0) {
+                sprite->ansi[offsetY + i][offsetX + j] = sprite->ansi[top + i][left + j];
+            }
+        }
+    }
+
+    // Fill the remaining area with whitespace and reset ansi
+    for (int i = 0; i < sprite->height; i++) {
+        for (int j = 0; j < sprite->width; j++) {
+            if (i < offsetY || i >= offsetY + newHeight || j < offsetX || j >= offsetX + newWidth) {
+                sprite->data[i][j] = ' ';
+                if (sprite->ansi != 0) {
+                    sprite->ansi[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    return 1;
+}
+
+int Sprite_center(CmdFX_Sprite* sprite) {
+    if (sprite == 0) return 0;
+    if (sprite->data == 0) return 0;
+
+    // Remove Sprite if Drawn
+    if (sprite->id != 0) Sprite_remove0(sprite);
+
+    if (!Sprite_center0(sprite)) {
+        Sprite_draw0(sprite);  // Redraw, center failed
+        return 0;
+    }
+
+    if (!Sprite_center0(sprite)) {
+        Sprite_draw0(sprite);  // Redraw, center failed
+        return 0;
+    }
+
+    // Redraw Sprite if Drawn
+    if (sprite->id != 0) Sprite_draw0(sprite);
+}
+
+int Sprite_resizeAndCenter(CmdFX_Sprite* sprite, int width, int height) {
+    if (sprite == 0) return 0;
+    if (width < 1 || height < 1) return 0;
+
+    // Remove Sprite if Drawn
+    if (sprite->id != 0) Sprite_remove0(sprite);
+
+    if (!Sprite_resize0(sprite, width, height, ' ')) {
+        Sprite_draw0(sprite); // Redraw, resize failed
+        return 0;
+    }
+
+    if (!Sprite_center0(sprite)) {
+        Sprite_draw0(sprite); // Redraw, center failed
+        return 0;
+    }
+
+    // Redraw Sprite if Drawn
+    if (sprite->id != 0) Sprite_draw0(sprite);
+
+    return 1;
 }
 
 // Utility Methods - Movement
