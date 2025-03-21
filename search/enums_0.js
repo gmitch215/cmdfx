222e47e94646d965ba396fcfffaab2cb0029c805
GitHub Action <action@github.com>
2025-03-21 15:18:46 +0000

    Update Docs (a6160f3)

--- search/enums_0.js
+++ search/enums_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cmdfx_5fgradientdirection_0',['CmdFX_GradientDirection',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7df',1,'sprites.h']]]
+  ['cmdfx_5fgradientdirection_0',['CmdFX_GradientDirection',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7df',1,'builder.h']]]
 ];
--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['gradient_5fangle_5f135_0',['GRADIENT_ANGLE_135',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfa4a6996c1f0f04ed54ff039897e269a6c',1,'sprites.h']]],
-  ['gradient_5fangle_5f45_1',['GRADIENT_ANGLE_45',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfa2dc8a2586ad9551a6722c403cfffd7ef',1,'sprites.h']]],
-  ['gradient_5fconical_2',['GRADIENT_CONICAL',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfac1494381af902afa592e47f702c4f155',1,'sprites.h']]],
-  ['gradient_5fhorizontal_3',['GRADIENT_HORIZONTAL',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfa408c389eb020def4ad24099940714914',1,'sprites.h']]],
-  ['gradient_5fhorizontal_5freverse_4',['GRADIENT_HORIZONTAL_REVERSE',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfaf4e997cfb5b1e7de85722d8599e53b9a',1,'sprites.h']]],
-  ['gradient_5fradial_5',['GRADIENT_RADIAL',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfa57d44d66677f7fe201cb9481df8e8a21',1,'sprites.h']]],
-  ['gradient_5fvertical_6',['GRADIENT_VERTICAL',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfaf2a6b598a6dc7b599e2a7e5d84f37bbf',1,'sprites.h']]],
-  ['gradient_5fvertical_5freverse_7',['GRADIENT_VERTICAL_REVERSE',['../d3/def/sprites_8h.html#a183207b41c93e1d43060730ec7a1d7dfa207db7b1fd266f16ec50196494a4d0ee',1,'sprites.h']]]
+  ['gradient_5fangle_5f135_0',['GRADIENT_ANGLE_135',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfa4a6996c1f0f04ed54ff039897e269a6c',1,'builder.h']]],
+  ['gradient_5fangle_5f45_1',['GRADIENT_ANGLE_45',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfa2dc8a2586ad9551a6722c403cfffd7ef',1,'builder.h']]],
+  ['gradient_5fconical_2',['GRADIENT_CONICAL',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfac1494381af902afa592e47f702c4f155',1,'builder.h']]],
+  ['gradient_5fhorizontal_3',['GRADIENT_HORIZONTAL',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfa408c389eb020def4ad24099940714914',1,'builder.h']]],
+  ['gradient_5fhorizontal_5freverse_4',['GRADIENT_HORIZONTAL_REVERSE',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfaf4e997cfb5b1e7de85722d8599e53b9a',1,'builder.h']]],
+  ['gradient_5fradial_5',['GRADIENT_RADIAL',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfa57d44d66677f7fe201cb9481df8e8a21',1,'builder.h']]],
+  ['gradient_5fvertical_6',['GRADIENT_VERTICAL',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfaf2a6b598a6dc7b599e2a7e5d84f37bbf',1,'builder.h']]],
+  ['gradient_5fvertical_5freverse_7',['GRADIENT_VERTICAL_REVERSE',['../d5/d78/builder_8h.html#a183207b41c93e1d43060730ec7a1d7dfa207db7b1fd266f16ec50196494a4d0ee',1,'builder.h']]]
 ];
