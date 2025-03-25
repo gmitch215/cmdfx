b2e0a22c132d4bd8b32105c7b91cd5624a31079e
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-03-24 21:10:12 -0500

    finish costume testing

--- test/src/core/costumes.c
+++ test/src/core/costumes.c
@@ -8,16 +8,37 @@
 int main() {
     int r = 0;
 
+    char** data1 = Char2DBuilder_createFilled(2, 2, '#');
     CmdFX_Sprite* sprite = Sprite_create(
-        Char2DBuilder_createFilled(2, 2, '#'),
+        data1,
         0, 0
     );
 
     CmdFX_SpriteCostumes* c1 = Sprite_createCostumes(sprite, 3);
     r |= assert(c1 != 0); 
 
-    Sprite_setCostumeAt(sprite, 1, Char2DBuilder_createFilled(2, 2, '@'), 0);
-    Sprite_setCostumeAt(sprite, 2, Char2DBuilder_createFilled(3, 3, '%'), String2DBuilder_createFilled(2, 2, "\033[31m"));
+    char** data2 = Char2DBuilder_createFilled(2, 2, '@');
+    char** data3 = Char2DBuilder_createFilled(3, 3, '%');
+    char*** ansi = String2DBuilder_createFilled(2, 2, "\033[31m");
+    Sprite_setCostumeAt(sprite, 1, data2, 0);
+    Sprite_setCostumeAt(sprite, 2, data3, ansi);
+
+    r |= assert(compareCharArrays(Sprite_getCharCostume(sprite, 0), data1) == 0);
+    r |= assert(compareStringArrays(Sprite_getAnsiCostume(sprite, 0), 0) == 0);
+    r |= assert(compareCharArrays(Sprite_getCharCostume(sprite, 1), data2) == 0);
+    r |= assert(compareCharArrays(Sprite_getCharCostume(sprite, 2), data3) == 0);
+    r |= assert(compareStringArrays(Sprite_getAnsiCostume(sprite, 2), ansi) == 0);
+
+    r |= assert(Sprite_switchCostumeTo(sprite, 1) == 0);
+    r |= assert(compareCharArrays(sprite->data, data2) == 0);
+    r |= assert(Sprite_switchCostumeTo(sprite, 2) == 0);
+    r |= assert(compareCharArrays(sprite->data, data3) == 0);
+    r |= assert(compareStringArrays(sprite->ansi, ansi) == 0);
+    r |= assert(Sprite_switchCostumeTo(sprite, 0) == 0);
+    r |= assert(compareCharArrays(sprite->data, data1) == 0);
+    r |= assert(compareStringArrays(sprite->ansi, 0) == 0);
+
+    Sprite_free(sprite);
 
     return r;
 }
